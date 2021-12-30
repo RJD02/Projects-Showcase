987aac7c1a7e595712c3ca36121b266221af3a86
RJD02 <redhood.dumb@gmail.com>
2021-12-30 09:39:45 +0530

    Fix source_link typo

--- projects/forms.py
+++ projects/forms.py
@@ -5,4 +5,4 @@ from .models import Project
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['title', 'description', 'demo_link', 'souce_link', 'tags']
+        fields = ['title', 'description', 'demo_link', 'source_link', 'tags']
