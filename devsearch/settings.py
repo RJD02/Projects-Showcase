3ad1247cc653a380c579efa10ca9db043bfe038b
RJD02 <dulangeraviraj@gmail.com>
2022-01-22 17:52:10 +0530

    Fix the config in settings

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'projects.apps.ProjectsConfig'
-    'users.apps.UsersConfig'
+    'projects.apps.ProjectsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
