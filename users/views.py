e2b14ba1cc65af4ddbe71ab807c559c659504c66
RJD02 <dulangeraviraj@gmail.com>
2022-01-25 11:01:40 +0530

    Filter the skills according to the description

--- users/views.py
+++ users/views.py
@@ -10,4 +10,17 @@ def profiles(request):
 
 
 def userProfile(request, pk):
-    return render(request, 'users/user-profile.html', )
+    profile = Profile.objects.get(id=pk)
+
+    # skills_with_description = [
+    #     skill for skill in profile.skill_set.all() if skill.description]
+    top_skills = profile.skill_set.exclude(description__exact='')
+
+    # other_skills = [
+    #     skill for skill in profile.skill_set.all() if skill.description == '']
+    other_skills = profile.skill_set.filter(description='')
+
+    context = {'profile': profile, 'top_skills': top_skills,
+               'other_skills': other_skills}
+
+    return render(request, 'users/user-profile.html', context)
