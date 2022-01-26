f1a71c2ccfc135529eae5840bbdcfa814d5aa023
RJD02 <dulangeraviraj@gmail.com>
2022-01-27 03:31:45 +0530

    Add register url

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,9 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_user, name='login'),
-
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+
 
     path('', views.profiles, name='profiles'),
     path('profile/<str:pk>/', views.userProfile, name='user-profile'),
