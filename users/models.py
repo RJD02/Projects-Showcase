085f6fc621b8052704925d83e257c128f95f8e9b
RJD02 <dulangeraviraj@gmail.com>
2022-01-26 13:42:16 +0530

    Remove signals from models.py to signals.py

--- users/models.py
+++ users/models.py
@@ -2,8 +2,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
+# Create your models here
 
 
 class Profile(models.Model):
@@ -43,23 +42,3 @@ class Skill(models.Model):
 
     def __str__(self) -> str:
         return (self.name)
-
-
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        user = instance
-        profile = Profile.objects.create(
-            user=user,
-            username=user.username,
-            email=user.email,
-            name=user.first_name,
-        )
-
-
-def delete_user(sender, instance, **kwargs):
-    user = instance.user
-    user.delete()
-
-
-post_save.connect(create_profile, sender=User)
-post_delete.connect(delete_user, sender=Profile)
