e5d3027abaef37fed2b5b99850a80bccad1769dc
RJD02 <dulangeraviraj@gmail.com>
2022-01-27 01:34:40 +0530

    Add registration/login and logout functionality

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login_user, name='login'),
+
+    path('logout/', views.logout_user, name='logout'),
+
     path('', views.profiles, name='profiles'),
     path('profile/<str:pk>/', views.userProfile, name='user-profile'),
 ]
