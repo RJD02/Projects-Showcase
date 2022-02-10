52247b8aefce892758d507ce77df07c1de6fc53e
RJD02 <dulangeraviraj@gmail.com>
2022-02-10 18:58:12 +0530

    CRUD urls for account and skill

--- users/urls.py
+++ users/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
 
     path('', views.profiles, name='profiles'),
     path('profile/<str:pk>/', views.userProfile, name='user-profile'),
+    path('account/', views.user_account, name='account'),
+    path('edit-account', views.edit_account, name='edit-account'),
+
+    path('add-skill/', views.create_skill, name='add-skill'),
+    path('update-skill/<str:pk>/', views.update_skill, name='update-skill'),
+    path('delete-skill/<str:pk>', views.delete_skill, name='delete-skill'),
 ]
