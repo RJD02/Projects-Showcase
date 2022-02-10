a77fce32e33a2d944b9fe2c6f8e730ad773cba45
RJD02 <dulangeraviraj@gmail.com>
2022-02-10 18:59:50 +0530

    Redirect to account once creation or updation operation is performed on a project

--- projects/views.py
+++ projects/views.py
@@ -7,14 +7,17 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
 def create_project(request):
+    profile = request.user.profile
     form = ProjectForm()
 
     print(request.POST)
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('projects')
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+            return redirect('account')
 
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
@@ -22,7 +25,9 @@ def create_project(request):
 
 @login_required(login_url='login')
 def update_project(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
+    # project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES, instance=project)
@@ -36,11 +41,13 @@ def update_project(request, pk):
 
 @login_required(login_url='login')
 def delete_project(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     if request.method == 'POST':
         project.delete()
-        return redirect('projects')
-    return render(request, 'projects/delete_template.html', {'object': project})
+        return redirect('account')
+    context = {'object': project, 'name': project}
+    return render(request, 'delete_template.html', context)
 
 
 def projects(request):
