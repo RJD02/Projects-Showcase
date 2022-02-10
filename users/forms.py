9929097c6eab2018f4497ea7c6111727439611a5
RJD02 <dulangeraviraj@gmail.com>
2022-02-10 18:58:27 +0530

    Add model form for profile and skill

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,10 @@
 from dataclasses import fields
+from pyexpat import model
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
+from users.models import Profile, Skill
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -15,6 +18,31 @@ class CustomUserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs) -> None:
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'username', 'location', 'email', 'about', 'bio', 'profile_image',
+                  'social_github', 'social_twitter', 'social_linkedin', 'social_youtube', 'social_website']
+
+    def __init__(self, *args, **kwargs) -> None:
+        super(ProfileForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class SkillForm(ModelForm):
+    class Meta:
+        model = Skill
+        fields = ['name', 'description']
+
+    def __init__(self, *args, **kwargs) -> None:
+        super(SkillForm, self).__init__(*args, **kwargs)
 
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
