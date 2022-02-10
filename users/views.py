394dd0c77b0bb5c7af46d6fed366f5c9a53922a9
RJD02 <dulangeraviraj@gmail.com>
2022-02-10 18:57:17 +0530

    Imports

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
-from .forms import CustomUserCreationForm
+from cmath import log
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from .models import Profile
+from .models import Profile, Skill
 from users.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
