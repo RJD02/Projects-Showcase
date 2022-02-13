6fa0d4b32185a86370c0dbcd8327d26d95b82e2f
RJD02 <dulangeraviraj@gmail.com>
2022-02-13 12:14:37 +0530

    Add logic for pagination for profiles

--- projects/utils.py
+++ projects/utils.py
@@ -2,6 +2,35 @@
 from projects.models import Project, Tag
 from django.db.models import Q
 
+from django.core.paginator import Paginator
+
+
+def paginate_projects(request, projects, results):
+    paginator = Paginator(projects, results)
+    page = 1
+    if request.GET.get('page'):
+        page = request.GET.get('page')
+        if int(page) > paginator.num_pages:
+            page = paginator.num_pages
+
+    projects = paginator.page(page)
+    page = int(page)
+
+    left_index = (page - 4)
+
+    if left_index < 1:
+        left_index = 1
+
+    right_index = (page + 5)
+    if page == 1:
+        right_index = page + 6
+
+    if right_index > paginator.num_pages:
+        right_index = paginator.num_pages
+
+    custom_range = range(left_index, right_index + 1)
+    return custom_range, projects
+
 
 def search_projects(request):
     search_query = ''
