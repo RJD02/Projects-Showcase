a183ab44392911dcfd84e75698a0b7711a396092
RJD02 <dulangeraviraj@gmail.com>
2022-02-24 13:26:56 +0530

    Make model form for Message

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,7 @@ from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-from users.models import Profile, Skill
+from users.models import Message, Profile, Skill
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -43,6 +43,18 @@ class SkillForm(ModelForm):
 
     def __init__(self, *args, **kwargs) -> None:
         super(SkillForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = ['name', 'email', 'subject', 'body']
+
+    def __init__(self, *args, **kwargs) -> None:
+        super(MessageForm, self).__init__(*args, **kwargs)
 
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
