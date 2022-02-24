e5637ecdf5759d5d04e36a9c9f9e964d377fbb4b
RJD02 <dulangeraviraj@gmail.com>
2022-02-24 11:43:18 +0530

    Make Message model

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, Skill
+from .models import Message, Profile, Skill
 
 admin.site.register(Profile)
 admin.site.register(Skill)
+admin.site.register(Message)
--- users/models.py
+++ users/models.py
@@ -42,3 +42,24 @@ class Skill(models.Model):
 
     def __str__(self) -> str:
         return (self.name)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, null=True, blank=True)
+    recipient = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name='messages')
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    is_read = models.BooleanField(default=False, null=True)
+    subject = models.CharField(max_length=200, null=True, blank=True)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.subject
+
+    class Meta:
+        ordering = ['is_read', '-created']
