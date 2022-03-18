46094a99daed39255c4dea88eb7672cc6062469a
RJD02 <dulangeraviraj@gmail.com>
2022-03-18 19:08:22 +0530

    Remove secret key

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^tsdn(&uuaas)au!uh@sk$3x-=(fztrcr0y=s$b6g2wsz^zs8f'
+# SECRET_KEY = 'django-insecure-^tsdn(&uuaas)au!uh@sk$3x-=(fztrcr0y=s$b6g2wsz^zs8f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
